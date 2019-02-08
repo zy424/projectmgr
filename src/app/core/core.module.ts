dd39225610009fa4516faa4185c2633d00f27bae
Yi Zhou <zhouyi0424@gmail.com>
2019-02-08 23:37:43 +1300

    UI layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,15 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+  ],
+  exports: [HeaderComponent, FooterComponent, SidebarComponent],
 })
 export class CoreModule {
   constructor (@Optional() @SkipSelf() parent: CoreModule) {
