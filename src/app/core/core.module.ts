e088b3d615ba2daf3574dd4e36dc84412ec2e448
Yi Zhou <zhouyi0424@gmail.com>
2019-02-09 23:19:27 +1300

    Angular material icon and svg

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,22 +1,32 @@
 import { NgModule, SkipSelf, Optional } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HttpClientModule} from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatToolbarModule, MatIconModule, MatButtonModule} from '@angular/material';
+import { MatIconRegistry} from '@angular/material';
+import { DomSanitizer } from '@angular/platform-browser';
+import { loadSvgResources } from '../utils/svg.utils';
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent, SidebarComponent],
   imports: [
     CommonModule,
+    HttpClientModule,
     MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   exports: [HeaderComponent, FooterComponent, SidebarComponent],
 })
 export class CoreModule {
-  constructor (@Optional() @SkipSelf() parent: CoreModule) {
+  constructor (@Optional() @SkipSelf() parent: CoreModule,
+               ir: MatIconRegistry,
+               ds: DomSanitizer) {
     if (parent) {
       throw new Error ('The module already exists, please do not load');
     }
+    loadSvgResources(ir, ds);
   }
 }
