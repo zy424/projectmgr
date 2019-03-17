e4a34e19f4bfedf0b65cdc7fd5a89da7060353a6
Yi Zhou <zhouyi0424@gmail.com>
2019-03-17 22:45:32 +1300

    Login Reactive form

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit() {
+    // this.form = new FormGroup({
+    //     email: new FormControl('a@123.com', Validators.compose([Validators.required, Validators.email])),
+    //     password: new FormControl('', Validators.required),
+    // });
+      this.form = this.fb.group({
+          email: ['wang@143.com', Validators.compose([Validators.required, Validators.email, this.validate])],
+          password: ['', Validators.required],
+      });
+  }
+  onSubmit({value, valid}, ev: Event) {
+      ev.preventDefault();
+      console.log(JSON.stringify(value));
+      console.log(JSON.stringify(valid));
+  }
+
+  validate(c: FormControl): {[key: string]: any} {
+      if (!c.value) {
+          return null;
+      }
+      const pattern = /^wang +/;
+      if (pattern.test(c.value)) {
+          return null;
+      }
+      return {
+          emailNotValid: 'The email must start with wang',
+      }
   }
 
 }
